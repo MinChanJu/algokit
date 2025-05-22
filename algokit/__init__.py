bdf720c8b092a04f24c863edeab5ed3474bd3796
MinChanJu <mcj00220077@gmail.com>
2025-05-22 14:26:36 +0900

    init 파일 수정

--- algokit/__init__.py
+++ algokit/__init__.py
@@ -1,22 +1,4 @@
-import os
-import importlib
+from . import graph
+from . import sort
 
-__all__ = []
-
-module_dir = os.path.dirname(__file__)
-
-for folder in os.listdir(module_dir):
-  folder_path = os.path.join(module_dir, folder)
-
-  # 딱 1단계 폴더 + __init__.py 가 있는 경우만 import
-  if (
-    os.path.isdir(folder_path)
-    and os.path.exists(os.path.join(folder_path, "__init__.py"))
-    and not folder.startswith("__")
-  ):
-    try:
-      module = importlib.import_module(f".{folder}", package=__name__)
-      globals()[folder] = module
-      __all__.append(folder)
-    except Exception as e:
-      print(f"[algokit] Failed to import subpackage: {folder}, error: {e}")
+__all__ = ['graph', 'sort']
--- algokit/graph/__init__.py
+++ algokit/graph/__init__.py
@@ -1,13 +1,5 @@
-import os
-import importlib
+from . import bfs
+from . import dfs
+from . import dijkstra
 
-__all__ = []
-
-module_dir = os.path.dirname(__file__)
-
-for filename in os.listdir(module_dir):
-	if filename.endswith(".py") and filename != "__init__.py":
-		modulename = filename[:-3]
-		module = importlib.import_module(f".{modulename}", package=__name__)
-		globals().update(vars(module))
-		__all__.append(modulename)
+__all__ = ['bfs', 'dfs', 'dijkstra']
